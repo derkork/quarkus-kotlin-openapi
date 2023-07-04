cecc0bed5b009356d6c3c576db589f066158b47a
Jan Thomä <j.thomae@tallence.com>
2023-07-04 09:35:55 +0200

    chore: initial commit

---
+++ src/test/kotlin/ReaderTest.kt
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test
+
+class ReaderTest {
+
+    @Test
+    fun readWorks() {
+        val inputStream = javaClass.getResourceAsStream("/example_openapi.json")!!
+        val jsonNode = read(inputStream).parseAsApiSpec()
+        println(jsonNode)
+    }
+}
