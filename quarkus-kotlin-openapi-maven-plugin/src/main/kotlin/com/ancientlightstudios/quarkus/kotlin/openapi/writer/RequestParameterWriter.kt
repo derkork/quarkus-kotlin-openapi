02503ea5491f070ab51ded3513135d7611565c79
Jan Thomä <janthomae@janthomae.de>
2023-08-28 16:31:40 +0200

    chore: implement server/client

--- quarkus-kotlin-openapi-maven-plugin/src/main/kotlin/com/ancientlightstudios/quarkus/kotlin/openapi/writer/RequestParameterWriter.kt
+++ quarkus-kotlin-openapi-maven-plugin/src/main/kotlin/com/ancientlightstudios/quarkus/kotlin/openapi/writer/RequestParameterWriter.kt
@@ -1,15 +1,27 @@
 package com.ancientlightstudios.quarkus.kotlin.openapi.writer
 
 import com.ancientlightstudios.quarkus.kotlin.openapi.GenerationContext
+import com.ancientlightstudios.quarkus.kotlin.openapi.ParameterKind
 import com.ancientlightstudios.quarkus.kotlin.openapi.RequestParameter
 import java.io.BufferedWriter
 
 fun RequestParameter.writeUnsafe(context: GenerationContext, writer: BufferedWriter) {
     val type = context.schemaRegistry.resolve(this.type)
+    writeAnnotation(context, writer)
     writer.write("${name.toKotlinIdentifier()}: ${type.toKotlinType(false)}?")
 }
 
 fun RequestParameter.writeSafe(context: GenerationContext, writer: BufferedWriter) {
     val type = context.schemaRegistry.resolve(this.type)
+    writeAnnotation(context, writer)
     writer.write("${name.toKotlinIdentifier()}: ${type.toKotlinType(true)}")
+}
+
+fun RequestParameter.writeAnnotation(context: GenerationContext, writer: BufferedWriter) {
+    when (kind) {
+        ParameterKind.PATH -> writer.write("@PathParam(\"{$name}\") ")
+        ParameterKind.QUERY -> writer.write("@QueryParam(\"$name\") ")
+        ParameterKind.HEADER -> writer.write("@HeaderParam(\"$name\") ")
+        ParameterKind.COOKIE -> writer.write("@CookieParam(\"$name\") ")
+    }
 }
--- quarkus-kotlin-openapi-test/src/main/kotlin/com/ancientlightstudios/example/NarfServer.kt
+++ quarkus-kotlin-openapi-test/src/main/kotlin/com/ancientlightstudios/example/NarfServer.kt
@@ -1,8 +1,20 @@
 package com.ancientlightstudios.example;
 
+import com.ancientlightstudios.example.model.MtxRequestSubscriptionModifyUnsafe
+import com.ancientlightstudios.example.model.MtxResponse
+import com.ancientlightstudios.example.model.MtxResponseSubscription
 import com.ancientlightstudios.example.server.NarfInterface
+import jakarta.enterprise.context.ApplicationScoped
+import jakarta.ws.rs.Path
+import java.util.UUID
 
+
+@ApplicationScoped
+@Path("/")
 class NarfServer : NarfInterface {
+    // lf4j logger
+    private val log = org.slf4j.LoggerFactory.getLogger(NarfServer::class.java)
+
     override suspend fun subscription_delete(
         filterName: String?,
         filter: String?,
@@ -14,8 +26,10 @@ class NarfServer : NarfInterface {
         deleteSession: Boolean?,
         TrafficRouteData: String?,
         SearchTerm: String?
-    ) {
-        TODO("Not yet implemented")
+    ): MtxResponse {
+        // log all input
+        log.info("subscription_delete: filterName=$filterName, filter=$filter, applyDefaultFilter=$applyDefaultFilter, ApiEventData=$ApiEventData, X_MATRIXX_SchemaNumber=$X_MATRIXX_SchemaNumber, X_MATRIXX_ExtensionNumber=$X_MATRIXX_ExtensionNumber, deleteDevice=$deleteDevice, deleteSession=$deleteSession, TrafficRouteData=$TrafficRouteData, SearchTerm=$SearchTerm")
+        return MtxResponse("subscription_delete", 27, 32, UUID.randomUUID().toString(), 0, "Klasse, alter!", 4711)
     }
 
     override suspend fun subscription_detail(
@@ -31,7 +45,7 @@ class NarfServer : NarfInterface {
         filterInvalidPurchasedOffer: Boolean?,
         filterInvalidBalance: Boolean?,
         SearchTerm: String?
-    ) {
+    ): MtxResponseSubscription {
         TODO("Not yet implemented")
     }
 
@@ -43,8 +57,10 @@ class NarfServer : NarfInterface {
         X_MATRIXX_SchemaNumber: String?,
         X_MATRIXX_ExtensionNumber: String?,
         TrafficRouteData: String?,
-        SearchTerm: String?
-    ) {
+        SearchTerm: String?,
+        body: MtxRequestSubscriptionModifyUnsafe?
+    ): MtxResponse {
         TODO("Not yet implemented")
     }
+
 }
