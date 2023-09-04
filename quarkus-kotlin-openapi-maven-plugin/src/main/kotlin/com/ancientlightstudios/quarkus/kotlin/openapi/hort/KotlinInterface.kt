b5f65c60f82107ca1f2b8916e5de71ac71330758
Maik Liebing <m.liebing@tallence.com>
2023-09-04 16:18:43 +0200

    feature: model

---
+++ quarkus-kotlin-openapi-maven-plugin/src/main/kotlin/com/ancientlightstudios/quarkus/kotlin/openapi/hort/KotlinInterface.kt
@@ -0,0 +1,8 @@
+package com.ancientlightstudios.quarkus.kotlin.openapi.hort
+
+class KotlinInterface(override val name: Name.ClassName) : KotlinFileContent {
+
+    override fun render() {
+        TODO("Not yet implemented")
+    }
+}
