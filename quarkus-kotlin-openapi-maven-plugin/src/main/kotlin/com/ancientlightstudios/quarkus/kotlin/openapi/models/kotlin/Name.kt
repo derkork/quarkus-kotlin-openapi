7c4428b521f58401995ca8a8fb68d029e3e0bd13
Maik Liebing <zloty@insomnia-hq.de>
2023-09-05 19:19:21 +0200

    chore: nice identifier names :)

--- quarkus-kotlin-openapi-maven-plugin/src/main/kotlin/com/ancientlightstudios/quarkus/kotlin/openapi/models/kotlin/Name.kt
+++ quarkus-kotlin-openapi-maven-plugin/src/main/kotlin/com/ancientlightstudios/quarkus/kotlin/openapi/models/kotlin/Name.kt
@@ -1,7 +1,6 @@
 package com.ancientlightstudios.quarkus.kotlin.openapi.models.kotlin
 
-import com.ancientlightstudios.quarkus.kotlin.openapi.strafbank.toKotlinClassName
-import com.ancientlightstudios.quarkus.kotlin.openapi.strafbank.toKotlinIdentifier
+import java.util.*
 
 sealed class Name(val name: String) {
 
@@ -9,4 +8,26 @@ sealed class Name(val name: String) {
     class VariableName(name: String) : Name(name.toKotlinIdentifier())
     class MethodName(name: String) : Name(name.toKotlinIdentifier())
 
+}
+
+private fun String.toKotlinClassName() = this.toKotlinIdentifier()
+    .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.ENGLISH) else it.toString() }
+
+private val cameCasePattern = Regex("([a-z])([A-Z])")
+private val unwantedGroupPattern = Regex("[^a-zA-Z0-9]+")
+private val snakeCasePattern = Regex("_([a-z])")
+
+private fun String.toKotlinIdentifier(): String {
+    // add an underscore into every combination of a lowercase letter followed by an uppercase letter
+    val cleaned = this.replace(cameCasePattern, "$1_$2")
+        // replace any sequence that is not a letter or a number with a single underscore
+        .replace(unwantedGroupPattern, "_")
+        // trim off any underscore at the start or end
+        .trim('_')
+        .lowercase()
+        // replace any letter directly behind an underscore with its uppercase buddy
+        .replace(snakeCasePattern) { it.groupValues[1].first().titlecase(Locale.ENGLISH) }
+
+    // if it does not start with a letter, prepend an underscore
+    return if (cleaned[0].isLetter()) cleaned else "_$cleaned"
 }
