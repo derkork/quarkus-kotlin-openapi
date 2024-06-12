ea67a61846efd7fa0ce8ba84901f2ebba0e3592c
Jan Thomä <janthomae@janthomae.de>
2024-06-12 14:59:35 +0200

    feat: generic error handling

--- quarkus-kotlin-openapi/src/main/kotlin/com/ancientlightstudios/quarkus/kotlin/openapi/IsError.kt
+++ quarkus-kotlin-openapi/src/main/kotlin/com/ancientlightstudios/quarkus/kotlin/openapi/IsError.kt
@@ -1,5 +1,16 @@
 package com.ancientlightstudios.quarkus.kotlin.openapi
 
 interface IsError {
-    val errorMessage:String
+    val errorMessage: String
 }
+
+interface IsTimeoutError : IsError
+interface IsUnreachableError : IsError
+
+interface IsConnectionResetError : IsError
+
+interface IsUnknownError : IsError {
+    val cause: Exception
+}
+
+interface IsResponseError : IsError
