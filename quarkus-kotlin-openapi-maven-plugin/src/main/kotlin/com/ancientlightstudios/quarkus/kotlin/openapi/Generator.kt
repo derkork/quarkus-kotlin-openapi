84354c6713002cc621b48899b817b634ca3b27dd
Maik Liebing <zloty@insomnia-hq.de>
2024-08-08 10:45:51 +0200

    feat: jsonata integration

--- quarkus-kotlin-openapi-maven-plugin/src/main/kotlin/com/ancientlightstudios/quarkus/kotlin/openapi/Generator.kt
+++ quarkus-kotlin-openapi-maven-plugin/src/main/kotlin/com/ancientlightstudios/quarkus/kotlin/openapi/Generator.kt
@@ -6,10 +6,17 @@ import com.ancientlightstudios.quarkus.kotlin.openapi.parser.ParserStage
 import com.ancientlightstudios.quarkus.kotlin.openapi.patching.PatchingStage
 import com.ancientlightstudios.quarkus.kotlin.openapi.refactoring.RefactoringStage
 import com.ancientlightstudios.quarkus.kotlin.openapi.validation.ValidationStage
+import org.slf4j.LoggerFactory
+import java.time.Duration
+import java.time.Instant
 
 class Generator(private val config: Config) {
 
+    private val log = LoggerFactory.getLogger(Generator::class.java)
+    
     fun generate() {
+        val start = Instant.now()
+
         val json = PatchingStage(config).process()
         val spec = TransformableSpec()
         ParserStage(config, json).process(spec)
@@ -17,6 +24,9 @@ class Generator(private val config: Config) {
         RefactoringStage(config).process(spec)
         // TODO: needs another validation stage. e.g. check that the enum-default value is a valid item
         EmitterStage(config).process(spec)
+
+        val duration = Duration.between(start, Instant.now())
+        log.info("Plugin took $duration")
     }
 
 }
--- quarkus-kotlin-openapi-maven-plugin/src/main/kotlin/com/ancientlightstudios/quarkus/kotlin/openapi/patching/PatchingStage.kt
+++ quarkus-kotlin-openapi-maven-plugin/src/main/kotlin/com/ancientlightstudios/quarkus/kotlin/openapi/patching/PatchingStage.kt
@@ -2,10 +2,13 @@ package com.ancientlightstudios.quarkus.kotlin.openapi.patching
 
 import com.ancientlightstudios.quarkus.kotlin.openapi.Config
 import com.ancientlightstudios.quarkus.kotlin.openapi.parser.merge
-import com.ancientlightstudios.quarkus.kotlin.openapi.parser.patch
+import com.ancientlightstudios.quarkus.kotlin.openapi.utils.ConfigIssue
+import com.dashjoin.jsonata.Jsonata.jsonata
+import com.dashjoin.jsonata.json.Json
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.flipkart.zjsonpatch.JsonPatch
 import java.io.File
 import kotlin.io.path.Path
 
@@ -15,13 +18,11 @@ class PatchingStage(private val config: Config) {
 
     fun process(): JsonNode {
         val mergedSource = config.sourceFiles
-            .map { read(it) }
+            .map { it.readAsJsonFile() }
             .reduce { acc, apiSpec -> acc.merge(apiSpec) }
 
         val patchedSource = config.patchFiles
-            .map { read(it) }
-            .fold(mergedSource) { document, patch -> document.patch(patch) }
-
+            .fold(mergedSource) { document, patch -> document.applyPatch(patch) }
 
         if (config.debugOutputFile != null) {
             Path(config.debugOutputFile).parent.toFile().mkdirs()
@@ -33,7 +34,29 @@ class PatchingStage(private val config: Config) {
         return patchedSource
     }
 
-    private fun read(source: String): JsonNode = File(source).inputStream().use { objectMapper.readTree(it) }
+    private fun JsonNode.applyPatch(patchFile: String): JsonNode {
+        val schema = patchFile.substringBefore("://", "jsonpatch")
+        val path = patchFile.substringAfter("://")
+
+        return when (schema) {
+            "jsonpatch" -> applyJsonPatch(path)
+            "jsonata" -> applyJsonataPatch(path)
+            else -> ConfigIssue("unsupported patch format $schema in $patchFile")
+        }
+    }
+
+    private fun JsonNode.applyJsonPatch(patchFile: String): JsonNode = JsonPatch.apply(patchFile.readAsJsonFile(), this)
+
+    private fun JsonNode.applyJsonataPatch(patchFile: String): JsonNode {
+        // jsonata doesn't know jackson, so we have to convert everything into plain objects first
+        var plainJson = objectMapper.convertValue(this, Any::class.java)
+        plainJson = jsonata(patchFile.readAsTextFile()).evaluate(plainJson)
+        // and convert it back into jackson's json nodes
+        return objectMapper.convertValue(plainJson, JsonNode::class.java)
+    }
+
+    private fun String.readAsTextFile() = File(this).readText()
+    private fun String.readAsJsonFile(): JsonNode = File(this).inputStream().use { objectMapper.readTree(it) }
 
     private fun patchJson(json: JsonNode) {
         ParameterSchemaPatch().process(json)
