14404ce2ac7d57841fbc8b38b679776af3a6d91c
Jan Thomä <j.thomae@tallence.com>
2024-08-14 14:45:44 +0200

    feat: make signal a CancellationException to better work with Arrow

--- quarkus-kotlin-openapi/src/main/kotlin/com/ancientlightstudios/quarkus/kotlin/openapi/RequestHandledSignal.kt
+++ quarkus-kotlin-openapi/src/main/kotlin/com/ancientlightstudios/quarkus/kotlin/openapi/RequestHandledSignal.kt
@@ -1,5 +1,19 @@
 package com.ancientlightstudios.quarkus.kotlin.openapi
 
 import org.jboss.resteasy.reactive.RestResponse
+import java.util.concurrent.CancellationException
 
-class RequestHandledSignal(val response: RestResponse<*>) : Exception(null, null, false, false)
+class RequestHandledSignal(val response: RestResponse<*>) :
+    CancellationException(
+        "This is a signal that the request was handled. This should not be catched. If catched, " +
+                "rethrow it, otherwise things will not work as intended."
+    ) {
+
+    // prevent creation of stack trace, as we don't need it and it hurts performance
+    override fun fillInStackTrace(): Throwable {  // copied from Arrow
+        // Prevent Android <= 6.0 bug.
+        stackTrace = emptyArray()
+        // We don't need stacktrace on shift, it hurts performance.
+        return this
+    }
+}
