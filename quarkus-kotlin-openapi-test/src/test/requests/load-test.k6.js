fd41f40eb132e3251a8eaed3d3d5094dbb361056
Maik Liebing <zloty@insomnia-hq.de>
2024-09-05 16:20:25 +0200

    feat: another way to build broken test data

--- quarkus-kotlin-openapi-test/src/test/requests/load-test.k6.js
+++ quarkus-kotlin-openapi-test/src/test/requests/load-test.k6.js
@@ -4,25 +4,8 @@ import {check, sleep} from 'k6'
 export default function () {
     const data =
         {
-            "title": "Iron Man",
-            "releaseDate": "2008-05-02",
-            "genres": [
-                "action",
-                "scifi"
-            ],
-            "duration": 126,
-            "cast": [
-                {
-                    "character": "Tony Stark",
-                    "actor": "Robert Downey Jr.",
-                    "type": "Person"
-                },
-                {
-                    "character": "Virginia Potts",
-                    "actor": "Gwyneth Paltrow",
-                    "type": "Person"
-                }
-            ]
+            "statusRequired": "second",
+            "itemsRequired": []
         }
 
     const params = {
@@ -31,7 +14,7 @@ export default function () {
         },
     }
 
-    let res = http.post('http://localhost:8080/movies', JSON.stringify(data), params)
+    let res = http.post('http://localhost:8080/features/json/required/object', JSON.stringify(data), params)
 
     check(res, {'success create': (r) => r.status === 200})
 }
--- quarkus-kotlin-openapi/src/main/kotlin/com/ancientlightstudios/quarkus/kotlin/openapi/UnsafeJson.kt
+++ quarkus-kotlin-openapi/src/main/kotlin/com/ancientlightstudios/quarkus/kotlin/openapi/UnsafeJson.kt
@@ -1,5 +1,18 @@
 package com.ancientlightstudios.quarkus.kotlin.openapi
 
 import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.ArrayNode
+import com.fasterxml.jackson.databind.node.ObjectNode
 
-class UnsafeJson<T>(val value: JsonNode)
+class UnsafeJson<T>(val value: JsonNode) {
+
+    fun modifyObject(block: ObjectNode.() -> ObjectNode) = when (value) {
+        is ObjectNode -> UnsafeJson<T>(value.run(block))
+        else -> throw IllegalStateException("object node expected")
+    }
+
+    fun modifyArray(block: ArrayNode.() -> ArrayNode) = when (value) {
+        is ArrayNode -> UnsafeJson<T>(value.run(block))
+        else -> throw IllegalStateException("array node expected")
+    }
+}
